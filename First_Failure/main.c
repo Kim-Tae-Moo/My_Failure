3f25e3a884a58b1a0bd9146a0c4580a1eebabbfe
Thompson_KOR <rlaxoan9327@jj.ac.kr>
2021-10-15 21:28:31 +0900

    Create main.c

---
+++ First_Failure/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include "First_Failure/Make_New_Memo.c"
+#include "First_Failure/Show_Memo_List.c"
+
+extern int Make_New_Memo();
+extern int Show_Memo_List();
+
+
+int MainMenu(void);
+
+int MainMenu(void)
+{
+        int option;
+        printf("\n-----------------\n");
+        printf("0.Exit Program\n");
+        printf("1.Make New Memo\n");
+        printf("2.Show Memo List\n");
+        printf("-----------------\n");
+        printf("option : ");
+        scanf("%d", &option);
+
+        if (option==0) return 0;
+        else if (option==1) Make_New_Memo();
+        else if (option==2) Show_Memo_List();
+        else printf("Program has shutdown.\n");
+        return 0;
+}
+
+int main(void)
+{
+        MainMenu();
+}
